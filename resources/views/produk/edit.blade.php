6d0dfb3060894592530d6af98d6cb00b6ade7fae
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-09-05 18:52:44 +0800

    finishing views for update method

--- resources/views/produk/edit.blade.php
+++ resources/views/produk/edit.blade.php
@@ -1,79 +1,57 @@
-@extends('layouts.app')
+@extends('layouts.app', ['title' => $produk->nama_produk])
 
 @section('content')
-		<div class="container">
-			<h2>Mengedit Data Produk</h2>
-			<!-- <br /> -->
-			@if ($errors->any())
-				<div class="alert alert-danger">
-					<ul>
-						@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-						@endforeach
-					</ul>
-				</div>
-				<!-- <br /> -->
-			@endif
-			@if (\Session::has('success'))
-				<div class="alert alert-success">
-					<p>{{ \Session::get('success') }}</p>
-				</div><br />
-			@endif
-			<form method="post" action="{{action('ProdukController@update',$id)}}" enctype="multipart/form-data">
-				{{csrf_field()}}
-				<input name="_method" type="hidden" value="PATCH">
-				<div class="row">
-					<div class="col-md-4"></div>
-					<div class="form-group col-md-4">
-						<label for="nama_produk">Nama produk:</label>
-						<input type="text" class="form-control" name="nama_produk" value="{{$produk->nama_produk}}">
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4"></div>
-					<div class="form-group col-md-4">
-						<label for="id_kategori">ID kategori:</label>
-						<!-- <input type="text" class="form-control" name="id_kategori" value="{{$produk->id_kategori}}"> -->
-						<select name="id_kategori" id="id_kategori" class="form-control input-sm">
-						@foreach($data_kategori as $kategori)
-							<option value="{{ $kategori->id_kategori }}" {{ $kategori->id_kategori === $produk->id_kategori ? 'selected' : ''}}>{{ $kategori->nama_kategori }}</option>
-						@endforeach
-						</select>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4"></div>
-					<div class="form-group col-md-4">
-						<label for="harga">Harga:</label>
-						<input type="text" class="form-control" name="harga" value="{{$produk->harga}}">
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4"></div>
-					<div class="form-group col-md-4">
-						<label for="deskripsi">Deskripsi:</label>
-						<textarea class="form-control" name="deskripsi" rows="3">{{$produk->deskripsi}}</textarea>
-						<!-- value="{{$produk->deskripsi}}" -->
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4"></div>
-					<div class="form-group col-md-4">
-						<label for="gambar">Gambar:</label>
-						<p>{{$produk->gambar}}</p>
-						<!-- <input type="text" class="form-control" name="gambar" value="{{$produk->gambar}}"> -->
-						<input type="file" class="form-control" name="gambar" id="gambar" value="{{$produk->gambar}}">
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4"></div>
-					<div class="form-group col-md-2">
-						<button type="submit" class="btn btn-success">Simpan Data Produk</button>
-					</div>
-					<div class="form-group col-md-2">
-						<a href="{{ URL::previous() }}" class="btn btn-primary">Cancel</a>
-					</div>
-				</div>
-			</form>
-		</div>
+<div class="row">
+  <div class="col-lg-12">
+    <div class="card py-3 px-3">
+      <form action="{{ route('produk.update', $produk->id_produk) }}" method="POST" enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="form-group">
+              <label for="nama_produk">Nama Produk</label>
+              <input type="text" value="{{ $produk->nama_produk }}" name="nama_produk" class="form-control" id="nama_produk" aria-describedby="emailHelp">
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="form-group">
+              <label for="id_kategori">Kategori</label>
+              <select class="form-control" name="id_kategori" id="id_kategori">
+                <option>Pilih kategori..</option>
+                @foreach($data_kategori as $kategori)
+                <option value="{{ $kategori->id_kategori }}" {{ $produk->kategori->nama_kategori === $kategori->nama_kategori ? 'selected' : '' }}>{{ $kategori->nama_kategori }}</option>
+                @endforeach
+              </select>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="form-group">
+              <label for="harga">Harga</label>
+              <input type="number" value="{{ $produk->harga }}" name="harga" class="form-control" id="harga" aria-describedby="emailHelp">
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">Deskripsi Produk</label>
+              <textarea class="form-control" name="deskripsi" id="exampleFormControlTextarea1" rows="3">{{ $produk->deskripsi }}</textarea>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="form-group">
+              <label for="gambar">Input gambar</label>
+              <input type="file" name="gambar" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+          </div>
+        </div>
+        <a href="{{ route('produk.index') }}" class="btn btn-secondary">Kembali</a>
+        <button type="submit" class="btn btn-primary">Ubah Data</button>
+      </form>
+    </div>
+  </div>
+</div>
 @endsection
