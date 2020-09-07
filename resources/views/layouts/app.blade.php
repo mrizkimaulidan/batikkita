0070866f2cf921a388016e0c0603752b58d0de8f
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-09-07 10:23:49 +0800

    change navbar brand title of the website according to .env file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,10 +50,10 @@
       <div class="main-sidebar">
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="{{ route('home') }}">{{ config('app.name') }}</a>
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="{{ route('home') }}">{{ str_limit(config('app.name'), 2, '') }}</a>
           </div>
           <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
